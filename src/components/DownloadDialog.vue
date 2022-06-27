b4f20e85f4f75e1cd8c9014bea18d375b6858911
Felix <felix.dziatkowski@googlemail.com>
2022-06-27 16:22:19 +0200

    add download dialog with strapi integration

--- src/components/DownloadDialog.vue
+++ src/components/DownloadDialog.vue
@@ -1,19 +1,38 @@
 <template>
     <div class="downloadDialog">
-        <div class="button">
-            <v-btn color="success" icon="mdi-domain" size="x-large" :elevation="isHovering ? 12 : 2"></v-btn>
-        </div>
+        <v-fade-transition>
+            <v-dialog>
+                <template v-slot:activator="{ props }">
+                    <div class="button">
+                        <v-btn v-bind="props" color="success" icon="mdi-domain" size="x-large"
+                            :elevation="isHovering ? 12 : 2"></v-btn>
+                    </div>
+                </template>
+                <template v-slot:default="{ isActive }">
+                    <v-card>
+                        <v-toolbar class="primary_color">Downloads</v-toolbar>
+                        <v-card-text class="secondary_color">
+                            <div class="text-h2 pa-12">Hier finden Sie die wichtigsten Dateien zu meiner Bewerbung</div>
+                            <DownloadDialogExpensionPanel />
+                        </v-card-text>
+                        <v-card-actions class="secondary_color justify-end">
+                            <v-btn text @click="isActive.value = false">Schließen</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </template>
+            </v-dialog>
+        </v-fade-transition>
     </div>
 </template>
 
 <script>
+import DownloadDialogExpensionPanel from './DownloadDialogExpensionPanel.vue'
 export default {
     name: "DownloadDialog",
     data: () => {
-        return {
-
-        }
-    }
+        return {};
+    },
+    components: { DownloadDialogExpensionPanel }
 }
 </script>
 
--- src/services/strapi.service.js
+++ src/services/strapi.service.js
@@ -3,8 +3,8 @@ const API_URL = process.env.VUE_APP_STRAPI_URL || "/";
 class StrapiAPI {
     constructor() {
     }
-    async getData(table) {
-        return fetch(API_URL + table + '?populate=*')
+    async getData(table, populate = "=*") {
+        return fetch(API_URL + table + '?populate' + populate)
             .then(response => response.json())
             .then(data => {
                 return data;
